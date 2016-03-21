999305312a6be4b6173fcc9b49f86304c0b54b7c
JaakkoL <jaakko.o.laurila@gmail.com>
2016-03-21 20:59:07 +0100

    Create npm package and first set of exportable rules.

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports = {
+  rules: {
+    'block-spacing': [2, 'always'],
+    'comma-style': [2, 'last'],
+    'curly': 2,
+    'eqeqeq': 2,
+    'indent': [2, 2, { SwitchCase: 1 }],
+    'key-spacing': [2, { 'beforeColon': false, 'afterColon': true }],
+    'linebreak-style': [2, 'unix'],
+    'max-depth': [2, 3],
+    'no-eval': 2,
+    'no-lone-blocks': 2,
+    'no-lonely-if': 2,
+    'no-trailing-spaces': 2,
+    'no-undefined': 2,
+    'no-unused-vars': [2, { 'args': 'none' }],
+    'quotes': [2, 'single'],
+    'semi': [2, 'never'],
+    'space-before-blocks': 2,
+    'space-before-function-paren': [2, 'never']
+  }
+}
